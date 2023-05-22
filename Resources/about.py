252afcf0e5eb77b0d2298beb1581efdc87537fbf
Sohi Earth <kkawamoto1017@gmail.com>
2023-05-22 20:19:20 +0900

    Lots of updates, new world_config feature. Updated version to 0.0.5, temp.

--- Resources/about.py
+++ Resources/about.py
@@ -1,7 +1,9 @@
 class Program_Info:
-    version = "a0.0.12"
+    version = "a0.0.5"
     channel = "Nightly"
-    name = "TextFS a0.0.12"
-    pdesc = "Lots of stuff."
-    changelog = ["Version 0.0.11 is a minor update to 0.0.1, including some bug fixes, and other things",
-                 "version 0.0.1: Initial release, no game, just menu. Sorry.","Done."]
+    name = "TextFS a0.0.5"
+    pdesc = "Main game is here. yay!."
+    changelog = ["Version 0.0.12: Lots of stuff.",
+                 "Version 0.0.11 is a minor update to 0.0.1, including some bug fixes, and other things",
+                 "version 0.0.1: Initial release, no game, just menu. Sorry.",
+                 "Done."]
