f88808d63af3fa0a7ea199561ea5a6c12f6e2f4b
Sohi Earth <kkawamoto1017@gmail.com>
2023-05-22 20:20:11 +0900

    Fixed about.py

--- Resources/about.py
+++ Resources/about.py
@@ -6,4 +6,4 @@ class Program_Info:
     changelog = ["Version 0.0.12: Lots of stuff.",
                  "Version 0.0.11 is a minor update to 0.0.1, including some bug fixes, and other things",
                  "version 0.0.1: Initial release, no game, just menu. Sorry.",
-                 '"Done."]
+                 "Done."]
