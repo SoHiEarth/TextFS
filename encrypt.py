c4cff16b697fd7d4310c27f089f355ef41c00cde
SoHiEarth <126925211+SoHiEarth@users.noreply.github.com>
2023-06-11 15:59:19 +0900

    Improved Encryption

--- encrypt.py
+++ encrypt.py
@@ -4,23 +4,40 @@ class keys:
         Code = random.choice(["01","001","99","999"])
         return Code
     def a():
-        Code = "11"
+        Code = random.choice(["11","011","89","989"])
         return Code
     def B():
-        Code = "02"
+        Code = random.choice(["02","002","98","998"])
         return Code
     def b():
-        Code = "12"
+        Code = random.choice(["12","012","88","988"])
         return Code
 def Encrypt(call):
     masterCode = ""
     for Letter in call:
         if Letter == A:
-            masterCode = masterCode + keys.A()
+            masterCode = masterCode + keys.A()+"|"
         if Letter == a:
-            masterCode = masterCode + keys.a()
+            masterCode = masterCode + keys.a()+"|"
         if Letter == B:
-            masterCode = masterCode + keys.B()
+            masterCode = masterCode + keys.B()+"|"
         if Letter == b:
-            masterCode = masterCode + keys.b()
+            masterCode = masterCode + keys.b()+"|"
     return masterCode
+def DeCrypt(Code):
+    masterCall = ""
+    currentCode = ""
+    for l in Code:
+        if l == "|":
+            if currentCode == "11" or currentCode == "011" or currentCode == "89" or currentCode == "989":
+                masterCall += "A"
+            elif currentCode == keys.a():
+                masterCall += "a"
+            elif currentCode == keys.B():
+                masterCall += "B"
+            elif currentCode == keys.b():
+                masterCall += "b"
+            currentCode = ""
+        else:
+            currentCode += l
+    return masterCall
