d4f09bc51853cce3a7c422f77cd63dcf13218ab2
SoHiEarth <126925211+SoHiEarth@users.noreply.github.com>
2023-06-20 20:54:19 +0900

    Updates

--- encrypt.py
+++ encrypt.py
@@ -4,24 +4,27 @@ class keys:
         Code = random.choice(["01","001","99","999"])
         return Code
     def a():
+        import random
         Code = random.choice(["11","011","89","989"])
         return Code
     def B():
+        import random
         Code = random.choice(["02","002","98","998"])
         return Code
     def b():
+        import random
         Code = random.choice(["12","012","88","988"])
         return Code
 def Encrypt(call):
     masterCode = ""
     for Letter in call:
-        if Letter == A:
+        if Letter == "A":
             masterCode = masterCode + keys.A()+"|"
-        if Letter == a:
+        if Letter == "a":
             masterCode = masterCode + keys.a()+"|"
-        if Letter == B:
+        if Letter == "B":
             masterCode = masterCode + keys.B()+"|"
-        if Letter == b:
+        if Letter == "b":
             masterCode = masterCode + keys.b()+"|"
     return masterCode
 def DeCrypt(Code):
